d322dabf9099645fd9d00d20e752027ee74e2246
private_face <private.face@gmail.com>
2021-06-01 23:29:47 +0200

    Fix relative URLs and add table of contents

--- build/post-process.js
+++ build/post-process.js
@@ -6,7 +6,7 @@ const { JSDOM } = jsdom;
 const MDN_URL = 'https://developer.mozilla.org';
 
 function getRelativePath(absoluteFrom, relativeTo, cwd, check = false) {
-    const from = absoluteFrom.endsWith('index.html')
+    const from = absoluteFrom.replace(/#.*$/, '').endsWith('/index.html')
         ? absoluteFrom.split(path.sep).slice(0, -1).join(path.sep)
         : absoluteFrom;
 
@@ -28,63 +28,86 @@ function getRelativePath(absoluteFrom, relativeTo, cwd, check = false) {
     return relativePath;
 }
 
-function fixStyle(filePath, documentsFolder, dom) {
+function fixUrls(dom, filePath, documentsFolder) {
     const document = dom.window.document;
+    const cssFiles = [];
 
-    // fix path to css
-    const style = document.querySelector('link[rel="stylesheet"]');
-    const styleHref = style.getAttribute('href');
-    style.setAttribute('href', getRelativePath(filePath, styleHref, documentsFolder));
-
-    // to fix paths in css later
-    return path.resolve(documentsFolder, styleHref.slice(1));
-}
-
-function fixLinks(filePath, documentsFolder, dom) {
-    const document = dom.window.document;
-
-    document.querySelectorAll('a[href]').forEach(node => {
-        const href = node.getAttribute('href');
-        if (!href.startsWith('/')) {
+    document.querySelectorAll('[href], [src]').forEach(node => {
+        if (node.nodeName === 'LINK' && node.getAttribute('rel').toLowerCase() === 'preload') {
+            return;
+        }
+        const attrName = node.hasAttribute('href') ? 'href' : 'src';
+        const url = node.getAttribute(attrName);
+        if (!url.startsWith('/')) {
+            // keep relative and external hrefs
             return;
         }
 
-        const relativePath = getRelativePath(filePath, href, documentsFolder, true);
-        const newHref = relativePath === null
-            ? MDN_URL + href
-            : relativePath.replace(/(#.*)?$/, '/index.html$1');
+        const isLink = node.nodeName === 'A';
+        const isCSS = node.nodeName === 'LINK' && node.getAttribute('rel').toLowerCase() === 'stylesheet';
 
-        node.setAttribute('href', newHref);
-        if (relativePath === null) {
+        const relativePath = getRelativePath(filePath, url, documentsFolder, true);
+        const targetFileExists = relativePath !== null;
+        const newUrl = !targetFileExists
+            ? MDN_URL + url
+            : isLink 
+            ? relativePath.replace(/(#.*)?$/, '/index.html$1')
+            : relativePath;
+
+        // mark relative paths that are not accessible as external
+        node.setAttribute(attrName, newUrl);
+        if (isLink && !targetFileExists) {
             node.classList.add('external');
         }
+
+        // console.log(`${node.nodeName}: "${url}" -> "${newUrl}" ${relativePath !== null ? '(file exists)' : ''}`);
+
+        if (isCSS && targetFileExists) {
+            cssFiles.push(path.resolve(documentsFolder, url.slice(1)));
+        }
+    });
+
+    return cssFiles;
+}
+
+function buildTableOfContents(dom) {
+    const document = dom.window.document;
+
+    document.querySelectorAll('h2 > a').forEach(node => {
+        const entryName = encodeURIComponent(node.textContent);
+        const a = document.createElement('a');
+        a.name = `//apple_ref/cpp/Section/${entryName}`;
+        a.className = 'dashAnchor';
+        node.before(a);
     });
 }
 
 module.exports = function postProcess(filePaths, documentsFolder) {
-    let cssPath;
+    let cssPaths = [];
 
     filePaths.forEach((filePath) => {
-        // console.log('Processing: ', filePath, '...');
-        const html = fs.readFileSync(filePath, 'utf8').toString();
+       const html = fs.readFileSync(filePath, 'utf8').toString();
         const dom = new JSDOM(html);
-
-        // fix css
-        cssPath = fixStyle(filePath, documentsFolder, dom);
-
-        // fix links
-        fixLinks(filePath, documentsFolder, dom);
-
-        // todo images
 
         // remove scripts (TODO it in yari)
         dom.window.document.querySelectorAll('script').forEach(node => node.remove());
 
+        // fix URLs in all tags, containing `href` or `src`
+        const css = fixUrls(dom, filePath, documentsFolder);
+        cssPaths = cssPaths.concat(css);
+
+        // build table of contents
+        buildTableOfContents(dom);
+
         fs.writeFileSync(filePath, dom.serialize());
     });
 
-    if (cssPath) {
+    // fix urls in CSS files
+    Array.from(new Set(cssPaths)).forEach(cssPath => {
+        if (!cssPath) {
+            return;
+        }
         const css = fs.readFileSync(cssPath, 'utf8').toString();
         fs.writeFileSync(cssPath, css.replace(/url\(\/static\//g, 'url(../'));
-    }
+    })
 };
